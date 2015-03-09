7d5293c4056fbe7dfa48c6948aaac505fbbcdffb
Anastasia Zorko <azorko13@gmail.com>
2015-03-08 18:33:20 -0700

    working skeleton

--- dropdown.js
+++ dropdown.js
@@ -1,5 +1,4 @@
 (function(rootObj) {
-	debugger
 	
 	var Dropdown = rootObj.Dropdown = rootObj.Dropdown || {};
 	
@@ -9,21 +8,25 @@
 		this.selectionsHash = createSelectionsHash(selections);
 		this.setupOptions(options);
 		this.createSelectElement(selections);
-		// this.prevQuery = "";
-		
-		// this.el.addEventListener("keyup", updateDropdown.bind(this));
 	};
 	
-	Widget.prototype.createSelectElement (selections) {
+	Widget.prototype.createSelectElement = function (selections) {
+		var frag = rootObj.document.createDocumentFragment();
 		var input = rootObj.document.createElement("input");
-		input.addEventListener("keyup", updateDropdown.bind(this));
 		var list = rootObj.document.createElement("ul");
 		var item;
+		
+		input.addEventListener("keyup", this.updateDropdown.bind(this));
+		frag.appendChild(input);
+		
 		selections.forEach( function (selection) {
 			item = rootObj.document.createElement("li");
+			item.innerText = selection;
 			list.appendChild(item);
 		});
-		this.el.appendChild(list);
+		
+		frag.appendChild(list);
+		this.el.appendChild(frag);
 	};
 	
 	function createSelectionsHash(selections) {
@@ -32,14 +35,15 @@
 		selections.forEach ( function (selection) {
 			selectionsHash[selection] = true;
 		});
-		return selectionsHash
+		return selectionsHash;	
 	};
 	
 	//could possibly only iterate through currently true keys if the new query contains the previous query
 	//sets selectionsHash[key] = true if matches query, and false otherwise
-	Widget.prototype.setMatches (query) { 
+	Widget.prototype.setMatches = function (query) {
+		var that = this;
 		Object.keys(this.selectionsHash).forEach( function (key) {
-			this.selectionsHash[key] = (key.search(RegExp(query)) === -1 ? false : true);
+			that.selectionsHash[key] = (key.search(RegExp(query)) === -1 ? false : true);
 		});
 	};
 	
@@ -50,18 +54,21 @@
 	};
 	
 	Widget.prototype.updateDropdown = function (event) {
-		var query = event.target.innerText;
-		var oldSelect = this.el.querySelector("select");
-		var selectClone = oldSelect.cloneNode(true);
+		var query = event.target.value;
+		var list = this.el.querySelector("ul");
+		var listClone = list.cloneNode(true);
+		var that = this, i, children = listClone.children, len = children.length, item;
 		
 		this.setMatches(query);
 		
-		this.selectClone.children.forEach ( function (option) {
-			//if option matches query
-			this.selectionsHash[option.innerText] ? option.classList.remove("display-none") : option.classList.add("display-none");
-		});
+		debugger
+	  for (i = 0; i < len; i++) {
+			item = children[i];
+			//if item matches query
+			that.selectionsHash[item.innerText] ? item.classList.remove("display-none") : item.classList.add("display-none");
+		}
 		
-		this.el.replaceChild(selectClone, oldSelect);
+		this.el.replaceChild(listClone, list);
 	};
 	
 })(this);
